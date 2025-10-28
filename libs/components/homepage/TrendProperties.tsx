8825d1726898f3959b8874bd087ba4d4a5b9a70f
sharif9318 <sharif.dilmuratovich@gmail.com>
2025-10-29 05:54:58 +0900

    fix: modify mobile homePage screen component

--- libs/components/homepage/TrendProperties.tsx
+++ libs/components/homepage/TrendProperties.tsx
@@ -87,7 +87,7 @@ const TrendProperties = (props: TrendPropertiesProps) => {
 								{trendProperties.map((property: Property) => {
 									return (
 										<SwiperSlide key={property._id} className={'trend-property-slide'}>
-											{/* <TrendPropertyCard property={property} likePropertyHandler={likePropertyHandler} /> */}
+											<TrendPropertyCard property={property} likePropertyHandler={likePropertyHandler} />
 										</SwiperSlide>
 									);
 								})}
